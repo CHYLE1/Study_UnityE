0f4d4768ce5b92ed3c3f12651f0cc680f26ef587
JunWoo PARK <zxcu513@naver.com>
2025-12-02 17:54:32 +0900

    Clean Upload: Unity project files

---
+++ Project_E/Assets/Script/Study.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    public string say = "";
+    public char text = 'a';
+    public int var01 = 1;
+    public float var02 = 0.4f;
+    public double var03 = 0.4;
+    public bool var04 = true; 
+
+    void Start()
+    {
+        Debug.Log(this.say);
+        Debug.Log(this.text);
+
+        int a = 10;
+
+        Debug.Log(a++);
+        Debug.Log(++a);
+
+        Debug.Log(Random.Range(0, 100));
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
